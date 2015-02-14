a3adc4dda590509ab151b99281a9a7c3ef90082c
lpww <t.treffry@gmail.com>
2015-02-13 20:04:27 -0800

    partically add collision detection

--- watchout.js
+++ watchout.js
@@ -1,27 +1,59 @@
 var difficulty = 30;
 var coords = [];
 var circles = 0;
+var count = 1;
 
-var selectedElement = 0;
-var currentX = 0;
-var currentY = 0;
-var currentMatrix = 0;
+var drag = d3.behavior.drag()
+    .on('dragstart', function() { player.style('fill', 'red'); })
+    .on('drag', function() { player.attr('cx', d3.event.x)
+    .attr('cy', d3.event.y); })
+    .on('dragend', function() { player.style('fill', 'white'); });
 
+var player = d3.select('svg').append('circle')
+    .attr('cx', 350)
+    .attr('cy', 250)
+    .attr('class', 'player')
+    .attr('r', 10)
+    .call(drag);
 
 var addCircle = function(x, y) {
-d3.select('svg').append('circle')
-  .attr('cx', x)
-  .attr('cy', y)
-  .attr('class', 'enemy')
-  .attr('r', 10);
+  d3.select('svg').append('circle')
+    .attr('cx', x)
+    .attr('cy', y)
+    .attr('class', 'enemy')
+    .attr('r', 10);
+}
+
+
+var collision = function(){
+  var enemies = d3.selectAll('.enemy');
+  var target = d3.select('.player');
+  for(var i = 0; i < enemies.length; i++){
+  console.log(target[i][0].cx.animVal.value);
+
+    if(Math.abs(enemies[i][0].cx.animVal.value - target[i][0].cx.animVal.value) < 20 && Math.abs(enemies[i][0].cy.animVal.value - target[i][0].cy.animVal.value) < 20){
+  console.log('fdg');
+target.style('fill', 'orange');
+    }
+  }
 }
 
 var animate = function() {
+  var color;
+  if (count %2===0) {
+    color = "purple";
+  }
+  else {
+    color = "blue";
+  }
+
   d3.selectAll('.enemy').data(coords).transition()
   .attr("cx", function(d) {return d[0];})
   .attr("cy", function(d) {return d[1];})
+  .style('fill', color)
   .duration(1000);
   shuffle(coords);
+  count++;
 }
 
 var shuffle = function(array){
@@ -38,48 +70,14 @@ var shuffle = function(array){
   }
 };
 
-var moveElement = function(evt){
-  // if(evt.clientX < 0) currentX = 0;
-  // if(evt.clientX > 700) currentX = evt.clientX;
-  dx = evt.clientX - currentX;
-  dy = evt.clientY - currentY;
-  currentMatrix[4] += dx;
-  currentMatrix[5] += dy;
-  newMatrix = "matrix(" + currentMatrix.join(' ') + ")";
-
-  selectedElement.setAttributeNS(null, "transform", newMatrix);
-  currentX = evt.clientX;
-  currentY = evt.clientY;
-}
-
-var selectPlayer = function(evt){
-  d3.select('.gameboard').attr('cursor', 'none');
-
-  selectedElement = evt.target;
-  currentX = evt.clientX;
-  currentY = evt.clientY;
-  currentMatrix = selectedElement.getAttributeNS(null, "transform").slice(7,-1).split(' ');
-
-  for(var i=0; i<currentMatrix.length; i++) {
-    currentMatrix[i] = parseFloat(currentMatrix[i]);
-  }
-
-  document.onmousemove = moveElement;
-  // d3.select('player').attr('onmousemove', 'moveElement');
-}
-
-d3.select('svg').append('circle')
-    .attr('cx', 350)
-    .attr('cy', 250)
-    .attr('class', 'player')
-    .attr('r', 10)
-    .attr('onmousedown', 'selectPlayer(evt)')
-    .attr('transform', 'matrix(1 0 0 1 0 0)');
-
 while(circles < difficulty){
  coords.push([Math.random()*750, Math.random()*550])
   addCircle(Math.random()*750, Math.random()*550);
   circles++;
 }
 
-setInterval(animate, 1000);
+setInterval(animate, 2000);
+setInterval(collision, 10);
+
+
+
