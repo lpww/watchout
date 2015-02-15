ed067d406830dc4c981c03813853df9c3735f486
lpww <t.treffry@gmail.com>
2015-02-14 17:32:47 -0800

    Add slider

--- watchout.js
+++ watchout.js
@@ -1,3 +1,4 @@
+//settings
 var color;
 var difficulty = 20;
 var coords = [];
@@ -7,6 +8,11 @@ var high = 0;
 var current =0;
 var collisions = 0;
 
+//slider variables
+var slider = d3.slider(25).axis(false).min(5).max(40);
+var value = d3.select('.slider').call(slider);
+
+//player
 var drag = d3.behavior.drag()
     .on('dragstart', function() { player.style('fill', 'white'); })
     .on('drag', function() {
@@ -17,10 +23,10 @@ var drag = d3.behavior.drag()
       } else {
         player.attr('cx', d3.event.x);
       }
-      if (d3.event.y < 110) {
-        player.attr('cy', 110);
-      } else if (d3.event.y > 590) {
-        player.attr('cy', 590);
+      if (d3.event.y < 10) {
+        player.attr('cy', 10);
+      } else if (d3.event.y > 490) {
+        player.attr('cy', 490);
       } else {
         player.attr('cy', d3.event.y);
       }
@@ -34,6 +40,7 @@ var player = d3.select('.gameboard').append('circle')
     .attr('r', 10)
     .call(drag);
 
+//add enemies
 var addCircle = function(x, y) {
   d3.select('.gameboard').append('circle')
     .attr('cx', x)
@@ -42,6 +49,16 @@ var addCircle = function(x, y) {
     .attr('r', 10);
 }
 
+//initialise
+var update = function(){
+  while(circles < difficulty){
+    coords.push([(Math.random()*750)-25, (Math.random()*550)-25])
+    addCircle(Math.random()*750, Math.random()*550);
+    circles++;
+  }
+};
+
+//scoreboard
 var score = function(){
   d3.select('.current').select('span').text(current++);
   if(current > high){
@@ -55,6 +72,7 @@ var score = function(){
   }
 }
 
+//Collision
 var collision = function(){
   var enemies = d3.selectAll('.enemy');
   var player = d3.select('.player');
@@ -62,12 +80,15 @@ var collision = function(){
   for(var i = 0; i < enemies[0].length; i++){
 
     if(Math.abs(enemies[0][i].cx.animVal.value - player[0][0].cx.animVal.value) < 19.5 && Math.abs(enemies[0][i].cy.animVal.value - player[0][0].cy.animVal.value) < 19.5){
+
       d3.selectAll('.scoreboard').style('color', color);
       player.style('fill', 'black');
+
       setTimeout(function(){
         d3.selectAll('.scoreboard').style('color', 'white');
         player.style('fill', 'white');
       }, 50);
+
       return true;
     }
   }
@@ -75,7 +96,15 @@ var collision = function(){
   return false;
 }
 
-var animate = function() {
+//Animations
+var shuffle = function(array){
+  coords = [];
+  for(var i = 0; i < difficulty; i++){
+    coords[i] = [(Math.random()*750)-25, (Math.random()*550)+25];
+  }
+};
+
+var animate = function(element) {
   if (count === 1) {
     color = "orange";
   }
@@ -86,12 +115,17 @@ var animate = function() {
     color = "yellow";
   }
 
-  d3.selectAll('.enemy').data(coords).transition()
-  .attr("cx", function(d) {return d[0];})
-  .attr("cy", function(d) {return d[1];})
-  .style('fill', color)
-  .duration(1000);
+  element.data(coords).transition()
+    .attr("cx", function(d) {return d[0];})
+    .attr("cy", function(d) {return d[1];})
+    .style('fill', color)
+    .duration(1000)
+    .each('end', function(){
+      animate(d3.select(this));
+  });
+
   shuffle(coords);
+
   if (count < 3) {
     count++;
   } else {
@@ -99,20 +133,10 @@ var animate = function() {
   }
 }
 
-var shuffle = function(array){
-  for(var i = 0; i < coords.length; i++){
-    coords[i] = [(Math.random()*750)-25, (Math.random()*550)+75];
-  }
-};
 
-while(circles < difficulty){
- coords.push([(Math.random()*750)-25, (Math.random()*550)+75])
-  addCircle(Math.random()*750, Math.random()*550);
-  circles++;
-}
-
-setInterval(animate, 1000);
-setInterval(score, 50);
+  update();
+  animate(d3.selectAll('.enemy'));
+  d3.timer(score);
 
 
 
